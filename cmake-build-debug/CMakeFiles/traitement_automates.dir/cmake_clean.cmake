217ccde34dea0bb33260b00d07388042575b1737
kim ⋆.ೃ࿔*:･ <kimyjoy@outlook.fr>
2025-03-22 18:48:37 +0100

    changement fichiers txt

---
+++ cmake-build-debug/CMakeFiles/traitement_automates.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/traitement_automates.dir/Automate.c.obj"
+  "CMakeFiles/traitement_automates.dir/Automate.c.obj.d"
+  "CMakeFiles/traitement_automates.dir/main.c.obj"
+  "CMakeFiles/traitement_automates.dir/main.c.obj.d"
+  "libtraitement_automates.dll.a"
+  "traitement_automates.exe"
+  "traitement_automates.exe.manifest"
+  "traitement_automates.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/traitement_automates.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
