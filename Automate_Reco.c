ee2818a924066fdc051e7d4e70ef9dffddce5968
trtony25 <150559400+trtony25@users.noreply.github.com>
2025-03-22 22:27:36 +0100

    Update Automate_Reco.c

--- Automate_Reco.c
+++ Automate_Reco.c
@@ -1,34 +1,46 @@
-#include "Automate_Reco.h"
-
-// Fonction pour lire une ligne complète de l'utilisateur
+// Fonction pour lire une ligne de mots
 void lire_ligne(char *ligne) {
     printf("Entrez une ligne de mots : ");
-    if (fgets(ligne, MAX_TAILLE_MOT, stdin) == NULL) {   // Sépare la ligne en mots en utilisant l'espace comme séparateur
-        fprintf(stderr, "Erreur de lecture\n");          // Copie le mot extrait dans le tableau des mots
+    if (fgets(ligne, MAX_TAILLE_MOT, stdin) == NULL) {
+        fprintf(stderr, "Erreur de lecture\n");
         exit(EXIT_FAILURE);
     }
+    ligne[strcspn(ligne, "\n")] = 0;
 }
 
-// Fonction pour séparer une ligne en mots
+// Fonction pour extraire les mots d'une ligne
 int extraire_mots(char *ligne, char mots[MAX_MOTS][MAX_TAILLE_MOT]) {
     int i = 0;
-    char *mot = strtok(ligne, " ");                      // Sépare la chaine en mots en utilisant l'espace comme séparateur
+    char *mot = strtok(ligne, " ");
     while (mot != NULL && i < MAX_MOTS) {
-        strcpy(mots[i], mot);                            // Copie le mot extrait dans le tableau mots
+        strcpy(mots[i], mot);
         i++;
         mot = strtok(NULL, " ");
     }
-    return i;                                            // Retourne le nombre de mots extraits
+    return i;
 }
 
-// Fonction pour vérifier si un mot est accepté par l'automate
-int reconnaitre_mot(automate *A, char *mot) {
-    int etat_courant = A->etats_initiaux[0];  
+// Vérifie si un état est terminal
+int est_terminal(automate_deterministe *A, int etat) {
+    for (int i = 0; i < A->nb_etats_terminaux; i++) {
+        for (int j = 0; j < A->taille_subset[i]; j++) {
+            if (A->etats_terminaux[i][j] == etat) {
+                return 1;  // L'état est terminal
+            }
+        }
+    }
+    return 0;
+}
+
+// Vérifie si un mot est accepté par l'automate
+int reconnaitre_mot(automate_deterministe *A, char *mot) {
+    int etat_courant = A->etat_initial[0];  // Un seul état initial (hypothèse)
     int i = 0;
 
-    while (mot[i] != '\0') {                                    // Parcours chaque caractère du mot
-        int transition_trouvee = 0;  
-        
+    while (mot[i] != '\0') {
+        int transition_trouvee = 0;
+
+        // Parcours du tableau de transitions
         for (int j = 0; j < A->nb_transition; j++) {
             if (A->transition->etat_depart[j] == etat_courant &&
                 A->transition->alphabet[j] == mot[i]) {
@@ -45,37 +57,26 @@ int reconnaitre_mot(automate *A, char *mot) {
     return est_terminal(A, etat_courant);  // Vérifie si on est dans un état terminal
 }
 
-// Fonction principale qui gère la reconnaissance des mots
-void boucle_reconnaissance(automate *A) {
-    char ligne[MAX_TAILLE_MOT];                    // Stocke la ligne entrée par l'utilisateur
-    char mots[MAX_MOTS][MAX_TAILLE_MOT];           // Tableau pour stocker les mots extraits
+// Boucle principale de reconnaissance
+void boucle_reconnaissance(automate_deterministe *A) {
+    char ligne[MAX_TAILLE_MOT];
+    char mots[MAX_MOTS][MAX_TAILLE_MOT];
 
-    while (1) { 
-        lire_ligne(ligne);                          // Demande une ligne à l'utilisateur
-        int nb_mots = extraire_mots(ligne, mots);   // Sépare la ligne en mots
+    while (1) {
+        lire_ligne(ligne);
+        int nb_mots = extraire_mots(ligne, mots);
 
-        for (int i = 0; i < nb_mots; i++) {         // Parcours tous les mots extraits
-            if (strcmp(mots[i], "fin") == 0) {      // Vérifie si le mot est "fin"
-                return; 
+        for (int i = 0; i < nb_mots; i++) {
+            if (strcmp(mots[i], "fin") == 0) {
+                return;
             }
 
-            if (reconnaitre_mot(A, mots[i])) {      // Vérifie si le mot est accepté ou non par l'automate
-                printf("oui\n");  
+            if (reconnaitre_mot(A, mots[i])) {
+                printf("oui\n");  // Mot accepté
             } else {
-                printf("non\n");  
+                printf("non\n");  // Mot rejeté
             }
         }
     }
 }
 
-// Vérifie si un état est terminal
-int est_terminal(automate_deterministe *A, int etat) {
-    for (int i = 0; i < A->nb_etats_terminaux; i++) {
-        for (int j = 0; j < A->taille_subset[i]; j++) {
-            if (A->etats_terminaux[i][j] == etat) {
-                return 1;  // L'état est terminal
-            }
-        }
-    }
-    return 0;
-}
