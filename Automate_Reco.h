908372eda252864046e8f5e797b241cc67e48937
trtony25 <150559400+trtony25@users.noreply.github.com>
2025-03-22 22:15:46 +0100

    Update Automate_Reco.h

--- Automate_Reco.h
+++ Automate_Reco.h
@@ -6,12 +6,15 @@
 #include "Automate.h"
 #include <string.h>
 
-#define MAX_MOTS 100
-#define MAX_TAILLE_MOT 100
+#define MAX_MOTS 50
+#define MAX_TAILLE_MOT 256
+#define MAX_ETATS 100
+#define MAX_TRANSITIONS 100
 
 void lire_ligne(char *ligne);
 int extraire_mots(char *ligne, char mots[MAX_MOTS][MAX_TAILLE_MOT]);
 int reconnaitre_mot(automate *A, char *mot);
 void boucle_reconnaissance(automate *A);
+int est_terminal(automate_deterministe *A, int etat);  
 
 #endif //TRAITEMENT_AUTOMATES_AUTOMATE_RECO_H
